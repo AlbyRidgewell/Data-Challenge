263c530f2d7ec6cf65bf3f824d3b8939631862c7
anaghanarayanan31 <117351780+anaghanarayanan31@users.noreply.github.com>
2022-12-08 14:30:04 +0000

    Update data_wrangling.R

--- data_wrangling.R
+++ data_wrangling.R
@@ -1,5 +1,10 @@
 # Data wrangling ##############################################################
 
 # Load data
+data0<-read.csv("heart_failure_clinical_records_dataset.csv")
+View(data0)
 
+# Checking for Missing Values
+is.na(data0)
+sum(is.na(data0))
 
